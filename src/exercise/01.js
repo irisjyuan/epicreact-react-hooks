c45f18ccb1a27f0efaa25d5858f28f7f57cd05bb
Iris Yuan <iris.yuan@captivateiq.com>
2023-01-10 16:52:38 -0800

    exercise 1 extra credit

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,9 +3,10 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting(initialName) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const [name, setName] = React.useState('')
+  console.log(initialName)
+  const [name, setName] = React.useState(initialName.initialName)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
@@ -17,7 +18,7 @@ function Greeting() {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -25,7 +26,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Iris" />
 }
 
 export default App
